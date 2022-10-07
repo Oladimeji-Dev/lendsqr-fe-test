15f471ffed8299937d82be640ae38fa4004e5c44
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-10-07 19:31:30 +0100

    chore: created the TableHead componet

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,36 +1,10 @@
 import './Table.scss'
-import filterIcon from '../../assets/filter.png'
+import { TableHead } from './TableHead/TableHead'
+
 export const Table = () => {
   return (
     <table className='table'>
-        <thead className='table-head'>
-            <tr className='table-head-row'>
-                <td className='table-head-row-data'>
-                    <span>organization</span>
-                    <img src={filterIcon} alt="filtericon" /> 
-                    </td>
-                <td>
-                    <span>Username</span>
-                    <img src={filterIcon} alt="filtericon" /> 
-                </td>
-                <td>
-                    <span>Email</span>
-                    <img src={filterIcon} alt="filtericon" /> 
-                </td>
-                <td>
-                    <span>Phone number</span>
-                    <img src={filterIcon} alt="filtericon" /> 
-                </td>
-                <td>
-                    <span>Date joined</span>
-                    <img src={filterIcon} alt="filtericon" /> 
-                </td>
-                <td>
-                    <span>Status</span>
-                    <img src={filterIcon} alt="filtericon" /> 
-                </td>
-            </tr>
-        </thead>
+        <TableHead />
     </table>
   )
 }
---
+++ src/components/Table/TableHead/TableHead.tsx
@@ -0,0 +1,33 @@
+import filterIcon from '../../assets/filter.png'
+export const TableHead = () => {
+  return (
+    <thead className='table-head'>
+        <tr className='table-head-row'>
+            <td className='table-head-row-data'>
+                <span>organization</span>
+                <img src={filterIcon} alt="filtericon" /> 
+                </td>
+            <td>
+                <span>Username</span>
+                <img src={filterIcon} alt="filtericon" /> 
+            </td>
+            <td>
+                <span>Email</span>
+                <img src={filterIcon} alt="filtericon" /> 
+            </td>
+            <td>
+                <span>Phone number</span>
+                <img src={filterIcon} alt="filtericon" /> 
+            </td>
+            <td>
+                <span>Date joined</span>
+                <img src={filterIcon} alt="filtericon" /> 
+            </td>
+            <td>
+                <span>Status</span>
+                <img src={filterIcon} alt="filtericon" /> 
+            </td>
+        </tr>
+        </thead>
+  )
+}
