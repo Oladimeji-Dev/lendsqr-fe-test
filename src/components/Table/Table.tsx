b26723d91c8d5614683ac1e7fb49e132e3f01b71
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-10-08 17:58:07 +0100

    feat:created the modal for the filter

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -4,7 +4,48 @@ import { TableHead } from './TableHead/TableHead'
 
 export const Table = () => {
   return (
-    <table className='table' width={"100%"} >
+    <div className='before-table'>
+        <div className='filter-data-lists'>
+            <form className="filter-data-form">
+                <div className="filter-data-list">
+                    <p>Organization</p>
+                    <select name="" id="">
+                        <option value="1">Select</option>
+                    </select>
+                </div>
+                <div className="filter-data-list">
+                    <p>Username</p>
+                    <input type="text" placeholder='Username' />
+                </div>
+                <div className="filter-data-list">
+                    <p>Email</p>
+                    <input type="text" placeholder='Email' />
+                </div>
+                <div className="filter-data-list">
+                    <p>Date</p>
+                    <input placeholder="Type Date" type="text"id="date"/>
+                </div>
+                <div className="filter-data-list">
+                    <p>Phone Number</p>
+                    <input type="text" placeholder='Phone Number'/>
+                </div>
+                <div className="filter-data-list">
+                    <p>Status</p>
+                    <select name="" id="">
+                        <option value="1">Select</option>
+                        <option value="Inactive">inactive</option>
+                        <option value="Active">Active</option>
+                        <option value="Blacklisted">Blacklisted</option>
+                    </select>
+                </div>
+                <div className="filter-data-list filter-data-list-buttons">
+                    <button className='filter-reset-btn'>Reset</button>
+                    <button className='filter-filter-btn' type='submit'>Filter</button>
+                </div>
+            </form>
+        </div>
+        
+        <table className='table' width={"100%"} >
         <TableHead />
         <tbody>
             <TableData/>
@@ -17,50 +58,8 @@ export const Table = () => {
             <TableData/>
             <TableData/>
         </tbody>
-        <div className="filter-data-lists">
-            <div className="filter-data-list">
-                <p>Organization</p>
-                <select name="" id="">
-                    <option value="1">Select</option>
-                </select>
-            </div>
-            <div className="filter-data-list">
-                <p>Organization</p>
-                <select name="" id="">
-                    <option value="1">Select</option>
-                </select>
-            </div>
-            <div className="filter-data-list">
-                <p>Organization</p>
-                <select name="" id="">
-                    <option value="1">Select</option>
-                </select>
-            </div>
-            <div className="filter-data-list">
-                <p>Organization</p>
-                <select name="" id="">
-                    <option value="1">Select</option>
-                </select>
-            </div>
-            <div className="filter-data-list">
-                <p>Organization</p>
-                <select name="" id="">
-                    <option value="1">Select</option>
-                </select>
-            </div>
-            <div className="filter-data-list">
-                <p>Organization</p>
-                <select name="" id="">
-                    <option value="1">Select</option>
-                </select>
-            </div>
-            <div className="filter-data-list">
-                <p>Organization</p>
-                <select name="" id="">
-                    <option value="1">Select</option>
-                </select>
-            </div>
-        </div>
     </table>
+    </div>
+    
   )
 }
