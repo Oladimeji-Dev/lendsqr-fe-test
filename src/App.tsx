73bef09fcf7b80491944a1c5c58a8db61d237d54
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-10-09 18:39:59 +0100

    feat: implemented the the browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import {Routes,Route} from 'react-router-dom'
 import { DashBoard } from './pages/DashBoard/DashBoard';
-
+import { Login } from './pages/Login/Login';
+import { UserDetails } from './pages/UserDetails/UserDetails';
 function App() {
   return (
     <div className="App">
-      {/* <UserDetails /> */}
-      <DashBoard />
+      <Routes>
+        <Route path='/' element ={<Login />} />
+        <Route path='/Dashboard' element ={<DashBoard />} />
+        <Route path='/User/:id' element ={<UserDetails />} />
+      </Routes>
+     
     </div>
   );
 }
