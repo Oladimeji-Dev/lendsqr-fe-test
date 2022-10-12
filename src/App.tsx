5bd708166c5ea0d934a663738a90440f6fdd8010
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-10-12 16:06:36 +0100

    chore: implemented the Private route with the Route

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,21 @@ import { Login } from './pages/Login/Login';
 import { UserDetails } from './pages/UserDetails/UserDetails';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import PrivateRoutes from './util/PrivateRoutes';
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path='/' element ={<Login />} />
-        <Route path='/users' element ={<DashBoard />} />
-        <Route path='/user/:id' element ={<UserDetails />} />
         <Route path='/*' element ={<h1>404 error paage not found</h1>} />
+        
+        {/* Private Route that cannot be access unless loged in */}
+        <Route path='/users' element ={<PrivateRoutes />}>
+          <Route path='/users' element ={<DashBoard />} />
+        </Route>
+        <Route path='/users' element ={<PrivateRoutes />}>
+          <Route path='/user/:id' element ={<UserDetails />} />
+        </Route>
       </Routes>
      <ToastContainer />
     </div>
